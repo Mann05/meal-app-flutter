d6e7f96a5e7c15a50fa260a89fe209876804c679
Mann Aikansh <aikanshmann@gmail.com>
2020-06-20 14:42:03 +0530

    remove the duplicate app bar in tabbar

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,23 +6,15 @@ import '../widgets/category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Meals Category',
-          style: Theme.of(context).textTheme.headline4,
-        ),
-      ),
-      body: Container(
-        margin: const EdgeInsets.all(10),
-        child: GridView(
-          children: dummy_Data.map((tx) => CategoryItem(tx)).toList(),
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-          ),
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: GridView(
+        children: dummy_Data.map((tx) => CategoryItem(tx)).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
         ),
       ),
     );
